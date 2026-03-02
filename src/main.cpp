cca9c4063860ee2f405d9be27b0a11537da1e2e4
Silvio <saccomansilvio@gmail.com>
2026-03-02 13:57:40 +0100

    Refactor audio streaming setup and dependencies in main.cpp and platformio.ini. Use of ESP8266Audio library

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,47 @@
-#include "Arduino.h"
-#include "Audio.h"
-#include "WiFi.h"
+#include <Arduino.h>
+#include <WiFi.h>
+#include "AudioFileSourceICYStream.h"
+#include "AudioFileSourceBuffer.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2S.h"
 
-// Definizione Pin I2S per MAX98357A
-#define I2S_LRC       25
-#define I2S_BCLK      26
-#define I2S_DOUT      22
+// Pin per MAX98357A
+#define I2S_BCLK 26
+#define I2S_LRC  25
+#define I2S_DOUT 22
 
-// Sostituisci con i tuoi dati
-const char* ssid = "Vodafone-E08636455";
-const char* password = "Spassocasa2025!";
-
-Audio audio; // 'true' uses internal RAM for buffer
+AudioGeneratorMP3 *mp3;
+AudioFileSourceICYStream *file;
+AudioFileSourceBuffer *buff;
+AudioOutputI2S *out;
 
 void setup() {
-    // Inizializza la seriale a 115200 baud
     Serial.begin(115200);
-    delay(1000); 
-    Serial.println("\n--- Avvio ESP32 Audio Player ---");
+    
+    WiFi.begin("Vodafone-E08636455", "Spassocasa2025!");
+    while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+    Serial.println("\nWiFi Connesso!");
 
-    // Tentativo di connessione WiFi
-    Serial.printf("Tentativo di connessione a: %s\n", ssid);
-    WiFi.begin(ssid, password);
+    // Configurazione Output I2S
+    out = new AudioOutputI2S();
+    out->setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+    out->setGain(0.1); // Volume iniziale basso (0.0 a 4.0)
 
-    // Loop di attesa con feedback visivo
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print("."); 
-    }
-
-    // Feedback di avvenuta connessione
-    Serial.println("\n[OK] WiFi Connesso!");
-    Serial.print("[INFO] Indirizzo IP assegnato: ");
-    Serial.println(WiFi.localIP());
-    Serial.print("[INFO] Potenza segnale (RSSI): ");
-    Serial.println(WiFi.RSSI());
-    Serial.println("---------------------------------");
-
-    // Configurazione Audio
-    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-    audio.setVolume(12); // Volume medio (0-21)
-
-    // Avvio streaming
-    Serial.println("[AUDIO] Avvio streaming radio...");
-    audio.connecttohost("http://stream.radioparadise.com/mp3_128");
+    // Configurazione Sorgente Radio (MP3)
+    file = new AudioFileSourceICYStream("http://stream.radioparadise.com/mp3_128");
+    
+    // Creiamo un buffer di 8KB (molto leggero per la RAM interna)
+    buff = new AudioFileSourceBuffer(file, 8192);
+    
+    mp3 = new AudioGeneratorMP3();
+    mp3->begin(buff, out);
 }
 
 void loop() {
-    // Importante: questo deve girare costantemente per alimentare il buffer audio
-    audio.loop();
-
-    // Feedback opzionale: se la connessione cade, avvisami
-    static unsigned long lastCheck = 0;
-    if (millis() - lastCheck > 10000) { // Controlla ogni 10 secondi
-        if (WiFi.status() != WL_CONNECTED) {
-            Serial.println("[ERRORE] WiFi disconnesso!");
-        }
-        lastCheck = millis();
+    if (mp3->isRunning()) {
+        if (!mp3->loop()) mp3->stop();
+    } else {
+        Serial.println("MP3 concluso o errore");
+        delay(1000);
     }
-}
-
-// Funzione di callback opzionale (la libreria la chiama automaticamente se presente)
-void audio_info(const char *info){
-    Serial.print("info        "); Serial.println(info);
 }
